f862cad6cc4e05d180266d595017762166360fa8
Raphael <raphaelarzoin173@gmail.com>
2024-05-29 13:34:49 +0200

    fini

--- js/ajouterAgenda.js
+++ js/ajouterAgenda.js
@@ -3,8 +3,8 @@ document.getElementById('addEventButton').addEventListener('click', function(eve
     const eventTitle = 'Bar-Mitsvah Gabriel';
     const eventLocation = '14 Rue des Rouges Gorges, Saint-Brice sous-foret';
     const eventDetails = 'Mise des téphilines de Gabriel.';
-    const eventStart = '20250323T083000Z';
-    const eventEnd = '20250323T113000Z';
+    const eventStart = '20250323T073000Z';
+    const eventEnd = '20250323T103000Z';
 
     const calendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(eventTitle)}&dates=${eventStart}/${eventEnd}&details=${encodeURIComponent(eventDetails)}&location=${encodeURIComponent(eventLocation)}&sf=true&output=xml`;
 
