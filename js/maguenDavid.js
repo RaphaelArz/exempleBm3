8e81aed2a1bc73ff345459e4e825c1a7ffd20265
Raphael <raphaelarzoin173@gmail.com>
2024-05-30 16:39:21 +0200

    maguen david on

--- js/maguenDavid.js
+++ js/maguenDavid.js
@@ -5,21 +5,25 @@ $(document).ready(function() {
 
         star.css({
             left: Math.random() * $(window).width() + 'px',
-            top: '-50px'
+            top: '-50px',
+            width: '30px', // Taille initiale
+            height: '30px' // Taille initiale
         });
 
         // Modifier la durée de l'animation (en millisecondes) ici
         star.animate({
-            top: $(window).height() + 'px'
-        }, 8000, 'linear', function() { // 8000 millisecondes = 8 secondes
+            top: $(window).height() + 'px',
+            width: '0px', // Taille finale
+            height: '0px' // Taille finale
+        }, 10000, 'linear', function() {
             $(this).remove();
         });
     }
 
     $('#invitationButton').click(function() {
-        for (let i = 0; i < 35; i++) { // Modifier le nombre d'étoiles ici
+        for (let i = 0; i < 50; i++) { // Modifier le nombre d'étoiles ici
             // Modifier le délai entre chaque création d'étoile (en millisecondes) ici
-            setTimeout(createStarOfDavid, i * 200); // 500 millisecondes = 0,5 seconde
+            setTimeout(createStarOfDavid, i * 200); // 200 millisecondes = 0,2 seconde
         }
     });
 });
